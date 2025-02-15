3feaf8b44031d8f19555f8031bcc93b503ca7f59
chany-ir <ci583241971>
2025-02-16 01:40:30 +0200

    ffffffddddddddddc

--- client/src/TasksService.js
+++ client/src/TasksService.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const apiUrl = process.env.REACT_APP_API_URL
+ const apiUrl = 'https://server-av10.onrender.com'
+//process.env.REACT_APP_API_URL
 axios.defaults.baseURL = apiUrl; 
 
 axios.interceptors.response.use(
